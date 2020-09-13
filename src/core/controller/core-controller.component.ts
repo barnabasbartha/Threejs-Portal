1fb8a5efab320beab5ba0f63842fd832a1be8b70
Barnabás Bartha <brw@barthaweb.com>
2020-09-13 12:39:29 +0200

    Migrate controller events to use RxJs

--- src/core/controller/core-controller.component.ts
+++ src/core/controller/core-controller.component.ts
@@ -1,10 +1,24 @@
 import {Singleton} from "typescript-ioc";
-import {CommonControllerComponent} from "../../common/controller/common-controller.component";
 import {KeyEvent} from "../../common/controller/controller.model";
-import {EventStatus} from "../../common/event.model";
+import {EventStatus, IVector2} from "../../common/event.model";
+import {Subject} from "rxjs";
 
 @Singleton
-export class CoreControllerComponent extends CommonControllerComponent {
+export class CoreControllerComponent {
+   private readonly resizeObject: IVector2 = {x: 0, y: 0};
+   private readonly resizeSubject = new Subject<IVector2>();
+   public readonly resize$ = this.resizeSubject.pipe();
+
+   private readonly mouseMoveObject: IVector2 = {x: 0, y: 0};
+   private readonly mouseMoveSubject = new Subject<IVector2>();
+   public readonly mouseMove$ = this.mouseMoveSubject.pipe();
+
+   private readonly keySubject = new Subject<KeyEvent>();
+   public readonly key$ = this.keySubject.pipe();
+
+   private readonly pointerLockSubject = new Subject<EventStatus>();
+   public readonly pointerLock$ = this.pointerLockSubject.pipe();
+
    resize(x: number, y: number) {
       this.resizeObject.x = x;
       this.resizeObject.y = y;
